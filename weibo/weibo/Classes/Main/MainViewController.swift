64e77a371f830f8fe64f9b7abb07e7251a73676c
why <why@163.com>
2019-05-21 11:55:42 +0800

    初始化项目:1.配置项目基本信息;2.创建项目的基本文件

---
+++ weibo/weibo/Classes/Main/MainViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MainViewController.swift
+//  weibo
+//
+//  Created by wuliangzhi on 2019/5/21.
+//  Copyright © 2019年 wuliangzhi. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ weibo/weibo/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  weibo
+//
+//  Created by wuliangzhi on 2019/5/21.
+//  Copyright © 2019年 wuliangzhi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.view.backgroundColor = UIColor.red;
+        
+    }
+
+
+}
+
